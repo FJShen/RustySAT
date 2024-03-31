b783981fee0a73c81241cfaea82fcba9fba5e32c
Yi En Gan <gan35@zahralablnx1.ecn.purdue.edu>
2024-03-31 19:19:36 -0400

    init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::env;
+
+mod parser;
 mod sat_solver;
+use crate::sat_solver::Problem;
 
 fn main() {
-    let p = sat_solver::get_sample_problem();
-    println!("problem is: {:#?}", p);
-    let solution = sat_solver::dpll::dpll(p);
-    println!("solution is {:?}", solution);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 1 {
+        panic!("Usage: cargo run <dimacs file>");
+    }
+
+    let mut ps = Vec::<Problem>::new();
+    // ps.push(sat_solver::get_sample_problem());
+    ps.push(parser::parse(&args[1]));
+
+    for p in ps {
+        println!("problem is: {:#?}", p);
+        let solution = sat_solver::dpll::dpll(p);
+        println!("solution is {:?}", solution);
+    }
 }
