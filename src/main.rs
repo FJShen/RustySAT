6bc93e7415886a8e65608d49a168b44daf3090cb
FJShen <shen449@purdue.edu>
2024-03-31 21:44:36 -0400

    main.rs panics if UNSAT

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod sat_solver;
 fn main() {
     let p = sat_solver::get_sample_problem();
     println!("problem is: {:#?}", p);
-    let solution = sat_solver::dpll::dpll(p);
+    let solution = sat_solver::dpll::dpll(p).unwrap();
     println!("solution is {:?}", solution);
 }
